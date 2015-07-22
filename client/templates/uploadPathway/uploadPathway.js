a0c0c704987c9e224d2cf08582183703d5963b7a
mokolodi1 <mokolodi1@gmail.com>
2015-07-21 18:23:37 -0700

    removed unnecessary files

--- client/templates/uploadPathway/uploadPathway.js
+++ client/templates/uploadPathway/uploadPathway.js
@@ -1,17 +1,11 @@
 Template.uploadPathway.events({
   "submit #upload-pathway": function (event, template) {
 
-    console.log("someone hit the submit button");
+    console.log("submit button hit");
 
     event.preventDefault(); // prevent default browser form submit
 
     var pathwayFile = event.target.pathwayFile.files[0];
-
-    Meteor.call("setUploadedFile", {
-      "fileName": pathwayFile.name,
-      "fileSize": pathwayFile.size,
-      "type": "pathway",
-    });
 
     UploadedFiles.insert(new FS.File(pathwayFile), function (error, fileObj) {
       console.log("done uploading the file");
